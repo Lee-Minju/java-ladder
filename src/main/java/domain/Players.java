03d70e97b15946746c21e9de9485cf47b88deacb
Lee-Minju <mju7513@gmail.com>
2024-01-31 00:03:34 +0900

    Players 클래스| validate메서드가 필드변수할당 전에 실행되도록 리팩터링

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -11,22 +11,22 @@ public class Players {
   private List<Player> players;
 
   public Players(List<Player> players) {
+    validatePlayers(players);
     this.players = players;
-    validatePlayers();
   }
 
-  private void validatePlayers() {
-    if (this.players.size() < MIN_PLAYERS_NUMBER) {
+  private void validatePlayers(List<Player> players) {
+    if (players.size() < MIN_PLAYERS_NUMBER) {
       throw new IllegalArgumentException("[ERROR] 플레이어 수는 두명 이상이어야 합니다.");
     }
-    hasDuplicatedName();
+    hasDuplicatedName(players);
   }
 
-  private void hasDuplicatedName() {
-    Set<Player> playersInSet = new HashSet<>();
-    for(int i=0; i<this.players.size(); i++){
-      playersInSet.add(this.getPlayerByIndex(i));
-      if(playersInSet.size() != i+1){
+  private void hasDuplicatedName(List<Player> players) {
+    Set<Player> duplicateChecker = new HashSet<>();
+    for(int i=0; i<players.size(); i++){
+      duplicateChecker.add(players.get(i));
+      if(duplicateChecker.size() != i+1){
         throw new IllegalArgumentException("[ERROR] 중복된 이름을 입력할 수 없습니다.");
       }
     }
