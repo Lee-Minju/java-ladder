04d1a8a91e990c199058a1f083b0da265cbfb103
Lee-Minju <mju7513@gmail.com>
2024-01-30 13:50:00 +0900

    Players/Player클래스 | 중복확인을 위한 메소드 구조 수정

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class Player {
 
   private Name playerName;
@@ -20,5 +22,19 @@ public class Player {
 
   public String getPlayerNameString() {
     return this.playerName.getNameValue();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if(!(obj instanceof Player)){
+      return false;
+    }
+    Player p = (Player) obj;
+    return (this.getPlayerNameString().equals(p.getPlayerNameString()));
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.getPlayerNameString());
   }
 }
--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -21,16 +21,16 @@ public class Players {
     if (this.numberOfPlayers < MIN_PLAYERS_NUMBER) {
       throw new IllegalArgumentException("[ERROR] 플레이어 수는 두명 이상이어야 합니다.");
     }
-    isDuplicate();
+    hasDuplicatedName();
   }
 
-  private void isDuplicate() {
-    Set<String> playersNameString = new HashSet<>();
+  private void hasDuplicatedName() {
+    Set<Player> playersInSet = new HashSet<>();
     for(int i=0; i<this.numberOfPlayers; i++){
-      playersNameString.add(this.getPlayerByIndex(i).getPlayerNameString());
-    }
-    if(playersNameString.size() != this.numberOfPlayers){
-      throw new IllegalArgumentException("[ERROR] 중복된 이름을 입력할 수 없습니다.");
+      playersInSet.add(this.getPlayerByIndex(i));
+      if(playersInSet.size() != i+1){
+        throw new IllegalArgumentException("[ERROR] 중복된 이름을 입력할 수 없습니다.");
+      }
     }
   }
 
@@ -41,4 +41,5 @@ public class Players {
   public Player getPlayerByIndex(int index) {
     return this.players.get(index);
   }
+
 }
